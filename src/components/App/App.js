6d7540b2474f94c07249f04426b6186676b56044
spencercoleman <spencer@edc.vc>
2022-05-14 14:23:17 -0700

    Add User route and component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
 const App = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const username = event.target[0].value;
+    
+    if (username) {
+      navigate(`/${username}`)
+    }
+  }
+
   return (
-    <div></div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Enter your GitHub Username</label>
+      <input name="username" type="text"></input>
+    </form>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './components/App/App';
+import User from './components/User';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/:userId" element={<User />} />
     </Routes>
   </BrowserRouter>
 );
