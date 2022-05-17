53d62f23981bd5679419c1cbcdcbba6b04fa1cb4
spencercoleman <spencer@edc.vc>
2022-05-17 16:32:13 -0700

    Improve UserForm tests

--- src/components/UserForm/UserForm.test.js
+++ src/components/UserForm/UserForm.test.js
@@ -14,6 +14,7 @@ describe('username form', () => {
         );
         expect(screen.getByLabelText('Enter your GitHub Username')).toBeInTheDocument();
     });
+
     it('renders text input for username', () => {
         render(
             <BrowserRouter>
@@ -22,4 +23,51 @@ describe('username form', () => {
         );
        expect(screen.getByRole('textbox')).toBeInTheDocument(); 
     });
+});
+
+describe('username input', () => {
+    it('calls onChange correct number of times on input', () => {
+        const onChangeMock = jest.fn();
+        render(
+            <BrowserRouter>
+                <UserForm setUsername={onChangeMock}/>
+            </BrowserRouter>
+        );
+        const input = screen.getByRole('textbox');
+        const username = 'spencercoleman';
+
+        userEvent.type(input, username);
+
+        expect(onChangeMock).toHaveBeenCalledTimes(username.length);
+    });
+
+    it('calls onChange with correct arguments on each input', () => {
+        const onChangeMock = jest.fn();
+        render(
+            <BrowserRouter>
+                <UserForm setUsername={onChangeMock}/>
+            </BrowserRouter>
+        );
+        const input = screen.getByRole('textbox');
+        const username = 'facebook';
+
+        userEvent.type(input, username);
+
+        expect(onChangeMock).toHaveBeenNthCalledWith(1, "f");
+        expect(onChangeMock).toHaveBeenNthCalledWith(3, "fac");
+        expect(onChangeMock).toHaveBeenNthCalledWith(8, "facebook");
+    });
+
+    it("input has correct values", () => {
+        const onChangeMock = jest.fn();
+        render(
+            <BrowserRouter>
+                <UserForm setUsername={onChangeMock}/>
+            </BrowserRouter>
+        );
+        const input = screen.getByRole("textbox");
+    
+        userEvent.type(input, "foobar");
+        expect(input).toHaveValue("foobar");
+      });
 });
