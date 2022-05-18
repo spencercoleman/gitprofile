00a28b071aed3ea75a2df2be7d72f38bc20eb760
spencercoleman <spencer@edc.vc>
2022-05-17 17:09:16 -0700

    Add basic user content

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -8,8 +8,31 @@ const User = () => {
     const [isLoading, fetchedData] = useFetch('https://api.github.com/users/' + params.userId);
 
     if (!isLoading && fetchedData) {
+        const user = fetchedData;
+        console.log(fetchedData);
+
         return (
-            <div>{fetchedData.login}</div>
+            <div>
+                <img src={user.avatar_url} alt={user.login}></img>
+                <h1>{user.name}</h1>
+                <h2><a href={user.html_url} target='_blank' rel='noreferrer'>@{user.login}</a></h2>
+                {user.blog && <div>{user.blog}</div>}
+                <ul>
+                    {user.company && <li>{user.company}</li>}
+                    {user.location && <li>{user.location}</li>}
+                    <li>{user.created_at}</li>
+                </ul>
+
+                {user.bio && <div>{user.bio}</div>}
+                <ul>
+                    <li>{user.public_repos} {user.public_repos === 1 ? 'repository' : 'repositories'}</li>
+                    <li>{user.public_gists} {user.public_gists === 1 ? 'gist' : 'gists'}</li>
+                </ul>
+                <ul>
+                    <li>{user.followers} {user.followers === 1 ? 'follower' : 'followers'}</li>
+                    <li>{user.following} following</li>
+                </ul>
+            </div>
         )
     }
     else if (!isLoading && !fetchedData) {
