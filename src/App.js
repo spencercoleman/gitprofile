242d232c5fbdef30059c2943fe0cf924696011a8
spencercoleman <spencer@edc.vc>
2022-05-24 02:00:14 -0700

    Refactor routes to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { useState } from "react";
+import {Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 import UserForm from "./components/UserForm/UserForm";
+import User from './components/User/User';
+import './App.css';
 
 const App = () => {
   const [username, setUsername] = useState('');
 
   return (
-    <UserForm username={username} setUsername={setUsername} />
+    <div className="App">
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<UserForm username={username} setUsername={setUsername} />} />
+          <Route path="/:userId" element={<User />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
-import User from './components/User/User';
-import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
-    <div className="container">
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/:userId" element={<User />} />
-        </Routes>
-    </div>
-
+    <App />
   </BrowserRouter>
 );
